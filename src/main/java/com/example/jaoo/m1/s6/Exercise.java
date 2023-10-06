f9ce71136d9a40e8495d170a7d081697c44d6f38
Agostino Marino <aiar@gft.com>
2023-10-06 18:23:04 +0200

    Exercises

--- src/main/java/com/example/jaoo/m1/s6/Exercise.java
+++ src/main/java/com/example/jaoo/m1/s6/Exercise.java
@@ -15,11 +15,31 @@ public class Exercise {
      * @param args used to test the code
      */
     public static void main(String[] args) {
-        if (args.length != 1) {
+      
+       int onlyDigit=0;
+      
+    	if (args.length != 1) {
             System.out.println("Pass me one argument!");
         } else {
             // TODO (1) check if the argument contains only digits
+        	for(int i=0;i<args[0].length();i++) {
+        		
+        		if(Character.isDigit(args[0].charAt(i))) {
+        			 onlyDigit++;
+        		}
+        	}
+        	
+        	if(onlyDigit==args[0].length()) {
+        		System.out.println("the argument contains only digits");
+        	}
+        	
+        	
+        	
             // TODO (2) check if the argument contains at least one digit
+        	
+        	if(onlyDigit>=1) {
+        		System.out.println("the argument contains at least one digits");
+        	}
 
             System.out.println("Done");
         }
